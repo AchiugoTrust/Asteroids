c0af7f9a73afc93b7b9a31bd5f114eed6a67f885
Achiugo Trust <achiugotrust@gmail.com>
2025-07-08 09:56:48 +0100

    player able to rotate

--- asteroids/main.py
+++ asteroids/main.py
@@ -2,17 +2,18 @@ import pygame
 from constants import *
 from player import *
 
-pygame.init()
-pygame.time.Clock()
-player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
-
-dt = 0
 
 
 
 
 def main():
+    pygame.init()
+    clock = pygame.time.Clock()
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    dt = 0
+
+    
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -24,11 +25,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        player.update(dt)
+
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
 
-    dt = 1000 / pygame.time.Clock.tick(60)
+        dt = 1000 / clock.tick(60)
 
 
     
